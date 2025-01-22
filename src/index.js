12dcba66b8f0f5aeab5b5642ae39c520fecc48dc
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2025-01-22 15:25:45 +0900

    make fib and merge function with its respectively recursive options

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import "./styles.css";
+function fibs(n) {
+  const array = [];
+  let a = 0;
+  let b = 1;
+
+  for (let i = 0; i < n; i++) {
+    array.push(a);
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return array;
+}
+
+function fibsRec(n, array = [0, 1]) {
+  console.log("This was printed recursively");
+  if (array.length >= n) {
+    return array.slice(0, n);
+  }
+
+  const nextNumber = array[array.length - 2] + array[array.length - 1];
+  return fibsRec(n, [...array, nextNumber]);
+}
+
+console.log(fibsRec(8));
